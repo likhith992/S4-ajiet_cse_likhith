ba84268bf689be0a8ed85746caf9b88e07fa954b
Likhith Shetty <237973421+likhith992@users.noreply.github.com>
2026-05-04 12:04:11 +0530

    Add integer swap program using XOR operation
    Implement a program to swap two integers using XOR.

---
+++ swap2.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=30;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
