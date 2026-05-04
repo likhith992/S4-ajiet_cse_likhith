b895c1187bb41c28ae1b9c55f6fabaa73dd2e88c
Likhith Shetty <237973421+likhith992@users.noreply.github.com>
2026-05-04 12:06:20 +0530

    Add swap program in swap3.java
    Implement a simple swap program to exchange values of two integers.

---
+++ swap3.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=30;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
