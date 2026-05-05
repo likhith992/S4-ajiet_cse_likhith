1cf2df921565525cc709555ab7ab0ae53652fcb7
Likhith Shetty <237973421+likhith992@users.noreply.github.com>
2026-05-05 15:46:15 +0530

    Implement whitespace removal in main method
    Remove all whitespace characters from the input string.

---
+++ remove spaces.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "l i k h i t h   i s    g o a t e d";
+        
+        
+        String result = text.replaceAll("\\s", "");
+        
+        System.out.println(result); 
+    }
+}
